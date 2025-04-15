40d36bda2f4cb4abfbf9c744282e3bd2ddf40609
sakata-mizuki <sakata_mizuki@y-i-groupinc.com>
2025-04-15 17:13:46 +0900

    complete rest api get

---
+++ src/main/java/com/college/yi/bookmanager/controller/BookApiController.java
@@ -0,0 +1,28 @@
+package com.college.yi.bookmanager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.college.yi.bookmanager.model.Book;
+import com.college.yi.bookmanager.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookApiController {
+
+    private final BookService bookService;
+
+    public BookApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok(books);
+    }
+}
---
+++ src/main/java/com/college/yi/bookmanager/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.college.yi.bookmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String showBooksPage() {
+        return "index"; // resources/templates/index.html を返す
+    }
+}
