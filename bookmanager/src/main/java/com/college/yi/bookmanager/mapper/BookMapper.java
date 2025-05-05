5c3d14138e01f9d7c89678e11fd23f3d83be37b8
syun2024 <78637482+syun2024@users.noreply.github.com>
2025-05-05 16:44:44 +0900

    Add:BookCRUD System

---
+++ bookmanager/src/main/java/com/college/yi/bookmanager/mapper/BookMapper.java
@@ -0,0 +1,32 @@
+package com.college.yi.bookmanager.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.college.yi.bookmanager.model.entity.BookEntity;
+
+@Mapper
+public interface BookMapper {
+
+    @Select("SELECT * FROM books")
+    List<BookEntity> findAll();
+
+    @Insert({ "INSERT INTO books(title, author, publisher, published_date, stock)",
+            "VALUES(#{title}, #{author}, #{publisher}, #{publishedDate}, #{stock})" })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(BookEntity book);
+
+    @Update({ "UPDATE books",
+            "SET title = #{title}, author = #{author}, publisher = #{publisher}, published_date = #{publishedDate}, stock = #{stock}",
+            "WHERE id = #{id}" })
+    int update(BookEntity book);
+
+    @Delete({ "DELETE FROM books", "WHERE id = #{id}" })
+    int delete(int id);
+}
--- bookmanager/src/main/java/com/college/yi/bookmanager/service/BookService.java
+++ bookmanager/src/main/java/com/college/yi/bookmanager/service/BookService.java
@@ -1,12 +1,15 @@
 package com.college.yi.bookmanager.service;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.college.yi.bookmanager.model.Book;
+import com.college.yi.bookmanager.model.entity.BookEntity;
+import com.college.yi.bookmanager.repository.BookRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,15 +17,56 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookService {
 
+    private final BookRepository bookRepository;
+
     public List<Book> getBooks() {
+
+        List<BookEntity> bookEntities = bookRepository.findAll();
+        if (bookEntities.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
         List<Book> bookList = new ArrayList<>();
-        bookList.add(new Book(1, "本１", "筆者１", "出版社１", LocalDate.now(), 100));
-        bookList.add(new Book(2, "本２", "筆者２", "出版社２", LocalDate.now(), 200));
-        bookList.add(new Book(3, "本３", "筆者３", "出版社３", LocalDate.now(), 300));
-        bookList.add(new Book(4, "本４", "筆者４", "出版社４", LocalDate.now(), 400));
-        bookList.add(new Book(5, "本５", "筆者５", "出版社５", LocalDate.now(), 500));
+        for (int i = 0; i < bookEntities.size(); i++) {
+
+            BookEntity bookEnt = bookEntities.get(i);
+            Book book = toModel(bookEnt);
+            bookList.add(book);
+        }
 
         return bookList;
+    }
+
+    public Book create(Book book) {
+        BookEntity bookEnt = toEntity(book);
+        bookRepository.insert(bookEnt);
+        return toModel(bookEnt);
+    }
+
+    public Book update(Book book) {
+        BookEntity bookEnt = toEntity(book);
+        bookRepository.update(bookEnt);
+        return toModel(bookEnt);
+    }
+
+    public void delete(int id) {
+        bookRepository.delete(id);
+    }
+
+    private BookEntity toEntity(Book book) {
+        BookEntity bookEnt = new BookEntity();
+        bookEnt.setId(book.getId());
+        bookEnt.setTitle(book.getTitle());
+        bookEnt.setAuthor(book.getAuthor());
+        bookEnt.setPublisher(book.getPublisher());
+        bookEnt.setPublishedDate(book.getPublishedDate());
+        bookEnt.setStock(book.getStock());
+        return bookEnt;
+    }
+
+    private Book toModel(BookEntity bookEnt) {
+        return new Book(bookEnt.getId(), bookEnt.getTitle(), bookEnt.getAuthor(), bookEnt.getPublisher(),
+                bookEnt.getPublishedDate(), bookEnt.getStock());
     }
 
 }
